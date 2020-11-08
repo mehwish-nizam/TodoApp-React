26c1226ca0817274f714a415d84288b8990dd598
Mehwish-nedian <mehwishnizam01@gmail.com>
2020-11-08 10:40:58 -0800

    Updates

---
+++ precache-manifest.a571ae7d207e528ae13b58dea37ab7e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ec653790bd51e75e5a3cef3b69c33c",
+    "url": "/TodoApp-React/index.html"
+  },
+  {
+    "revision": "c40d71820e5cab149645",
+    "url": "/TodoApp-React/static/css/main.6124d2ea.chunk.css"
+  },
+  {
+    "revision": "c789eb0a6133747afa95",
+    "url": "/TodoApp-React/static/js/2.633dc908.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoApp-React/static/js/2.633dc908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c40d71820e5cab149645",
+    "url": "/TodoApp-React/static/js/main.0c88c2f3.chunk.js"
+  },
+  {
+    "revision": "1d257a8b4f32799af94b",
+    "url": "/TodoApp-React/static/js/runtime-main.0607be4d.js"
+  }
+]);
